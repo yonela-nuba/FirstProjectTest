d6534501ca7cf430871cac90d84c951bbc8111ba
yonela-nuba <ynuba@rosebankcollege.co.za>
2024-09-09 07:28:32 +0000

    First commit

---
+++ src/main/java/com/mycompany/firstprojecttogithub/FirstProjectToGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstprojecttogithub;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FirstProjectToGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
