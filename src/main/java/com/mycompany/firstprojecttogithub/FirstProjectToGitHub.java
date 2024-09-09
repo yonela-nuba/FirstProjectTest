bb86a210f39e29fc933021e0528edb80356e5849
yonela-nuba <ynuba@rosebankcollege.co.za>
2024-09-09 08:23:09 +0000

    Created a program to add two numbers

--- src/main/java/com/mycompany/firstprojecttogithub/FirstProjectToGitHub.java
+++ src/main/java/com/mycompany/firstprojecttogithub/FirstProjectToGitHub.java
@@ -11,6 +11,9 @@ package com.mycompany.firstprojecttogithub;
 public class FirstProjectToGitHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 5;
+        int b = 10;
+        int c = a+ b;
+        System.out.println("Sum : " + c);
     }
 }
